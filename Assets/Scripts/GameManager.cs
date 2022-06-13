92b75c90e3e1dd9908cebceec0194617a094dde8
Ashbarf <scarespin@gmail.com>
2022-06-13 21:55:17 +0800

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    int score;
+    public static GameManager inst;
+
+    [SerializeField] Text scoreText;
+
+    [SerializeField] PlayerMovement playerMovement;
+
+    public void IncrementScore ()
+    {
+        score = score + 10;
+        scoreText.text = "Score: " + score;
+        // Increase the player's speed
+        playerMovement.speed += playerMovement.speedIncreasePerPoint;
+    }
+
+    private void Awake ()
+    {
+        inst = this;
+    }
+
+    private void Start () {
+
+	}
+
+	private void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GroundTile.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class GroundTile : MonoBehaviour {
+
+    GroundSpawner groundSpawner;
+    [SerializeField] GameObject coinPrefab;
+    [SerializeField] GameObject obstaclePrefab;
+    [SerializeField] GameObject wallPrefab;
+
+    private void Start () {
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+	}
+
+    private void OnTriggerExit (Collider other)
+    {
+        groundSpawner.SpawnTile(true);
+        Destroy(gameObject, 2);
+    }
+
+    public void SpawnObstacle ()
+    {
+        // Choose a random point to spawn the obstacle
+        int obstacleSpawnIndex = Random.Range(2, 4);
+        Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
+
+        // Spawn the obstace at the position
+        Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
+    }
+
+    public void SpawnWall()
+    {
+        // Wall Position Left & Right
+        Transform spawnPointLeft = transform.GetChild(5).transform;
+        Transform spawnPointRight = transform.GetChild(6).transform;
+
+        // Spawn the wall
+        Instantiate(wallPrefab, spawnPointLeft.position, Quaternion.identity, transform);
+        Instantiate(wallPrefab, spawnPointRight.position, Quaternion.identity, transform);
+    }
+
+    public void SpawnCoins ()
+    {
+        int coinsToSpawn = 1;
+        for (int i = 0; i < coinsToSpawn; i++) {
+            GameObject temp = Instantiate(coinPrefab, transform);
+            temp.transform.position = GetRandomPointInCollider(GetComponent<Collider>());
+        }
+    }
+
+    Vector3 GetRandomPointInCollider (Collider collider)
+    {
+        Vector3 point = new Vector3(
+            Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+            Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+            Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+            );
+        if (point != collider.ClosestPoint(point)) {
+            point = GetRandomPointInCollider(collider);
+        }
+
+        point.y = 1;
+        return point;
+    }
+}
